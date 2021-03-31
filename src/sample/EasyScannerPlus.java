67533066261f1bc10bc1feaeb8804a039b6f2749
alexfilho19 <alexandrofilho33@hotmail.com>
2021-03-31 10:31:00 +0100

    " "

--- src/sample/EasyScannerPlus.java
+++ src/sample/EasyScannerPlus.java
@@ -1,10 +1,16 @@
+/**
+ * This class has been designed to improve the user's experience by
+ * making the keyboard input easier. All methods have been made as
+ * static so that we can simply use the class name when we call a
+ * method.
+ */
 package sample;
-
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class EasyScannerPlus  {
 
+    //Method to hold integers
     public static int nextInt() {
         Scanner keyboard = new Scanner(System.in);
         int i;
@@ -17,6 +23,7 @@ public class EasyScannerPlus  {
         return i;
     }
 
+    //Method to hold doubles
     public static double nextDouble() {
         Scanner keyboard = new Scanner(System.in);
         double d;
@@ -28,12 +35,14 @@ public class EasyScannerPlus  {
         return d;
     }
 
+    //Method to hold Strings
     public static String nextString() {
         Scanner keyboard = new Scanner(System.in);
         String s = keyboard.nextLine();
         return s;
     }
 
+    //Method to hold characters
     public static char nextChar() {
         Scanner keyboard = new Scanner(System.in);
         char c = keyboard.next().charAt(0);
